6ef8ae89dbf8269602f3a3420b9cdd97c47c244b
Michael Pérez <michaelperezvezoli@hotmail.com>
2024-08-28 17:19:33 -0300

    added meta tags to head

--- src/features/posts/PostPage.js
+++ src/features/posts/PostPage.js
@@ -20,6 +20,35 @@ const PostPage = () => {
         }),
     })
 
+    useEffect(() => {
+        //setTimeout(() => {
+            if (post?.title) {
+                console.log(post)
+                const headElement = document.getElementsByTagName('head')[0]
+                const meta1 = document.createElement('meta')
+                meta1.setAttribute('property', 'og:title')
+                meta1.setAttribute('content', `${post?.title}`)
+                const meta2 = document.createElement('meta')
+                meta2.setAttribute('property', 'og:type')
+                meta2.setAttribute('content', 'article')
+                const meta3 = document.createElement('meta')
+                meta3.setAttribute('property', 'og:description')
+                meta3.setAttribute('content', `${post?.heading}`)
+                const meta4 = document.createElement('meta')
+                meta4.setAttribute('property', 'og:url')
+                meta4.setAttribute('content', `${`https://oncemetros.onrender.com/post/${post?.searchField}`}`)
+                const meta5 = document.createElement('meta')
+                meta5.setAttribute('property', 'og:image')
+                meta5.setAttribute('content', `https://oncemetros.onrender.com/Images/${post?.searchField}.jpg`)
+                headElement.appendChild(meta1)
+                headElement.appendChild(meta2)
+                headElement.appendChild(meta3)
+                headElement.appendChild(meta4)
+                headElement.appendChild(meta5)
+            }
+        //}, 2000)
+    }, [post])
+
     if (post) {
         return (
             <>
