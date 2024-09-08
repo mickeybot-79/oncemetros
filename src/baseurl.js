d7807c273d7cd1b2e404ac7a122171c4bc031ab5
Michael Pérez <michaelperezvezoli@hotmail.com>
2024-09-08 09:54:06 -0300

    expanded user page style, corrected base url

--- src/baseurl.js
+++ src/baseurl.js
@@ -1,11 +1,11 @@
-const baseUrl = {
-    backend: 'http://localhost:3500',
-    frontend: 'http://localhost:3000'
-}
-
 // const baseUrl = {
-//     backend: 'https://one1mbackend.onrender.com',
-//     frontend: 'hhttps://oncemetros.onrender.com'
+//     backend: 'http://localhost:3500',
+//     frontend: 'http://localhost:3000'
 // }
+
+const baseUrl = {
+    backend: 'https://one1mbackend.onrender.com',
+    frontend: 'hhttps://oncemetros.onrender.com'
+}
 
 export default baseUrl
